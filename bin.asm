6f693f3ba5779bcc897ac9dfe31205e98eed78f4
dydx <al.dighton@outlook.com>
2019-07-06 01:47:11 +0500

    Working version of bin.asm with more comments added

--- bin.asm
+++ bin.asm
@@ -1,85 +1,85 @@
 .data
-	msg1:	.asciiz "Enter decimal number: "
-	msg2:	.asciiz "The IEEE-754 Single Precision equivalent is: "
+msg1:	.asciiz "Enter decimal number: "
+msg2:	.asciiz "The IEEE-754 Single Precision equivalent is: "
 
 .text
 
 .globl main
 .ent main
 main:	
-	addi $v0, $zero, 4
-	la $a0, msg1
-	syscall					# prints msg1 to console
+addi $v0, $zero, 4
+la $a0, msg1
+syscall					# prints msg1 to console
 
-	addi $v0, $zero, 5
-	syscall					# takes integer input 
-	add $s0, $zero, $v0		# saves input in $s0
+addi $v0, $zero, 5
+syscall					# takes integer input 
+add $s0, $zero, $v0		# saves input in $s0
 
-	addi $v0, $zero, 4
-	la $a0, msg2
-	syscall					# prints msg2 to console
+addi $v0, $zero, 4
+la $a0, msg2
+syscall					# prints msg2 to console
 
-	jal SIGN
+jal SIGN
 
-	li $v0, 1
-	add $a0, $a0, $zero	
-	syscall					# displays the value of $a0
+li $v0, 1
+add $a0, $a0, $zero	
+syscall					# displays the value of $a0
 
-	jal ZEXCP
+jal ZEXCP
 
-	add $s7, $zero, $s0		# $s7 = $s0
+add $s7, $zero, $s0     # $s7 = $s0
 
-	jal LEN
+jal LEN
 
-	addi $s0, $s1, 127		# add 127 to the length of binary input in $s0
-	add $s2, $zero, $s0		# which is the exponent so essentially
-							# $s0 = 127 + exp
-	jal LEN
+addi $s0, $s1, 127      # add 127 to the length of binary input in $s0
+add $s2, $zero, $s0     # which is the exponent so essentially
+# s0 = 127 + exp
+jal LEN
 
-	add $s0, $zero, $s2		# $s0 = $s2
-	li $t0, 128
+add $s0, $zero, $s2		# $s0 = $s2
+li $t0, 128
 
-	addi $v0, $zero, 1
+addi $v0, $zero, 1
 
-	jal BIN
+jal BIN
 
-	add $s0, $zero, $s7
-	add $s2, $zero, $s0
+add $s0, $zero, $s7
+add $s2, $zero, $s0
 
-	jal LEN
+jal LEN
 
-	addi $t0, $zero, 1
-	add $s0, $zero, $s2
+addi $t0, $zero, 1
+add $s0, $zero, $s2
 
-	add $s7, $zero, $s1
+add $s7, $zero, $s1
 
-	addi $s1, $s1, -1
-	sll $t0, $t0, $s1
+addi $s1, $s1, -1
+sll $t0, $t0, $s1
 
-	jal BIN
+jal BIN
 
-	add $s6, $zero, 23
-	sub $s7, $s6, $s7
-	add $a0, $zero, $zero
+add $s6, $zero, 23
+sub $s7, $s6, $s7
+add $a0, $zero, $zero
 
-	jal REP
+jal REP
 
-	addi $v0, $zero, 10
-	syscall
+addi $v0, $zero, 10
+syscall
 .end main
 
 
 .globl ZEXCP
 .ent ZEXCP
 ZEXCP:
-	bne $s0, $zero, ZEXCPL
-	add $a0, $zero, $zero
+bne $s0, $zero, ZEXCPL
+add $a0, $zero, $zero
 
-	li $v0, 10
-	syscall
-	
+li $v0, 10
+syscall
+
 ZEXCPL:	
-	jr $ra
+jr $ra
 
 .end ZEXCP
 
@@ -87,64 +87,66 @@ ZEXCPL:
 .globl SIGN
 .ent SIGN
 SIGN:	
-	slt $t0, $s0, $zero		# checks if the input is less than zero
-	bne $t0, $zero, NEG 	# jumps to NEG if $t0 != 0 as input < 0 
+slt $t0, $s0, $zero		# checks if the input is less than zero
+bne $t0, $zero, NEG 	# jumps to NEG if $t0 != 0 as input < 0 
 
-	add $a0, $zero, $zero	# saves $a0 = 0
+add $a0, $zero, $zero	# saves $a0 = 0
 
-	jr $ra
+jr $ra
 
 NEG:	
-	addi $t0, $zero, 2		# since $t0 = 1, hence $t0 = 3
-	mult $s0, $t0			# multiplies input with 3
-	mflo $t0				# saves product in $t0
-	sub $s0, $s0, $t0		# this essentially makes a -ve into +ve
+addi $t0, $zero, 2		# since $t0 = 1, hence $t0 = 3
+mult $s0, $t0			# multiplies input with 3
+mflo $t0				# saves product in $t0
+sub $s0, $s0, $t0		# this essentially makes a -ve into +ve
 
-	addi $a0, $zero, 1		# saves $a0 = 1
+addi $a0, $zero, 1		# saves $a0 = 1
 
-	jr $ra
+jr $ra
 .end SIGN
 
 
 .globl LEN
 .ent LEN
 LEN:	
-	add $s1, $zero, $zero	# initializes $s1 = 0
-	addi $t0, $zero, 2		
+add $s1, $zero, $zero
+addi $t0, $zero, 2
 
 LLOOP:	
-	div $s0, $t0			
-	mflo $s0				# storing division quotient in $s0
-	beq $s0, $zero, LENL	# this block of code essentially calculates
-	addi $s1, $s1, 1		# the length of converted binary number
-	j LLOOP					# using a loop
+div $s0, $t0
+mflo $s0
+beq $s0, $zero, LENL
+addi $s1, $s1, 1
+j LLOOP
 
 LENL:	
-	jr $ra
+jr $ra
 .end LEN
 
 
 .globl BIN
 .ent BIN
 BIN:	
-	li $s3, 1
-	add $a0, $s3, $zero
-	syscall
-	addi $s1, $s1, -1
-	srl $t0, $t0, 1
-	beq $t0, $zero, BINL
-	j BIN
+and $s3, $t0, $s0
+srl $s3, $s3, $s1
+add $a0, $s3, $zero
+syscall
+addi $s1, $s1, -1
+srl $t0, $t0, 1
+beq $t0, $zero, BINL
+j BIN
 
-BINL:	jr $ra
+BINL:	
+jr $ra
 .end BIN
 
 
 .globl REP
 .ent REP
 REP:	syscall
-	addi $s7, $s7, -1
-	beq $s7, $zero, REPL
-	j REP
+addi $s7, $s7, -1
+beq $s7, $zero, REPL
+j REP
 
 REPL:	jr $ra
 .end REP
